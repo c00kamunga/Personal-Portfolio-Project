82e37b7d01d8d5571f2c536d8fb5f1e309c74cf9
c00kamunga <tsothalanti2@yahoo.com>
2019-11-25 19:14:07 -0800

    created navigation component

---
+++ person/src/Components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Main() {
+    return (
+        <div>
+            <h1>About</h1>
+            <h1>Skills</h1>
+            <h1>Projects</h1>
+            <h1>Contact</h1>
+        </div>
+    )
+}
