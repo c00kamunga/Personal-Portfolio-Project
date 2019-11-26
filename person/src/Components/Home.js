7b3cdbc241d8f417fb88004e3741f9721f8c6217
c00kamunga <tsothalanti2@yahoo.com>
2019-11-25 18:35:15 -0800

    rendered Home component to the page, added ProjectCards Component file

---
+++ person/src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <p>Hi,
+                I'm Barinder Joseph,
+                An Aspiring Web Developer
+            </p>
+
+        </div>
+    )
+}
+
+
+
